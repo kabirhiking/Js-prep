9f6ed6f85ba384567c4ae1ee51fc039a137ac8f7
kabir <kabirraihan249@gmail.com>
2025-06-23 15:07:34 +0600

    primitive value types

---
+++ js_tutorial.js
@@ -0,0 +1,27 @@
+//Primitive /Value Types
+
+// String
+ let x = "edu";
+ console.log(x);
+ console.log(typeof(x));
+ x = 5;
+ console.log(x);
+
+ //number
+ let y = 1;
+ console.log(y);
+ console.log(typeof(y));
+
+
+//boolean
+ let z = true;
+ console.log(z);
+ console.log(typeof(z));
+
+ //undefined
+ let w;
+ console.log(w);
+ w=9;
+ console.log(typeof(w));
+
+
